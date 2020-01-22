2d5a2f2461087e506b9a8671047b6d6b1816772e
firmo holanda <firmo@viize.com>
2020-01-22 19:27:32 -0300

    Update rest_client.rb

--- rest_client.rb
+++ rest_client.rb
@@ -7,32 +7,31 @@ require 'nokogiri'
 # search for it
 # return the results
 class BingSearch
-
   URL = 'https://www.bing.com/search?q='
-  
+
   def search
-    puts "Enter the keyword you want to search for: "
-    @@key_word = gets.chomp
-    word = "#{URL}#{@@key_word}"
-    @@response = RestClient.get word
+    puts 'Enter the keyword you want to search for: '
+    @key_word = gets.chomp
+    word = "#{URL}#{@key_word}"
+    @response = RestClient.get word
   end
 
   def show
-    puts "code:"    + @@response.code.to_s
-    puts "headers:" + @@response.headers.to_s
-    puts "cookies:" + @@response.cookies.to_s
-    puts "history:" + @@response.history.to_s
-    puts "body:"    + @@response.body
+    puts 'code:'    + @response.code.to_s
+    puts 'headers:' + @response.headers.to_s
+    puts 'cookies:' + @response.cookies.to_s
+    puts 'history:' + @response.history.to_s
+    puts 'body:'    + @response.body
   end
 
   def save_files
     # write file dump file to local dir
-    File.write('body.html', @@response.body.to_s)
+    File.write('body.html', @response.body.to_s)
   end
 
   def display__search_links
     # parse html with nokogiri
-    puts @parsed_data = Nokogiri::HTML.parse(@@response.body.to_s)
+    puts @parsed_data = Nokogiri::HTML.parse(@response.body.to_s)
 
     # get all lins from response.body
     nodeset = @parsed_data.xpath('//a')
