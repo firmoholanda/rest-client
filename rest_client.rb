fc2a2e9b91091b364007aa19e04a12da0be215f3
firmo holanda <firmo@viize.com>
2020-01-22 18:24:49 -0300

    add nokogiri parser and extract links

--- rest_client.rb
+++ rest_client.rb
@@ -1,4 +1,5 @@
 require 'rest-client'
+require 'nokogiri'
 
 response = RestClient.get "https://www.bing.com/search?q=rails"
 
@@ -8,4 +9,15 @@ response = RestClient.get "https://www.bing.com/search?q=rails"
 #puts "history:" + response.history.to_s
 #puts "body:"    + response.body
 
+# write file dump file to local dir
 File.write('body.html', response.body.to_s)
+
+# parse html with nokogiri
+parsed_data = Nokogiri::HTML.parse(response.body.to_s)
+
+# get search title
+puts parsed_data.title
+
+# get all lins from response.body
+nodeset = parsed_data.xpath('//a')      
+puts nodeset.map {|element| element["href"]}.compact
