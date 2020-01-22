062b8d39dc39f81ab9c9e3f3168f2a4b3bd34d9e
firmo holanda <firmo@viize.com>
2020-01-22 17:37:20 -0300

    Create rest_client.rb

---
+++ rest_client.rb
@@ -0,0 +1,9 @@
+require 'rest-client'
+
+response = RestClient.get "https://www.bing.com/search?q=rails"
+
+puts "code:"    + response.code.to_s
+#puts "headers:" + response.headers.to_s
+#puts "cookies:" + response.cookies.to_s
+#puts "history:" + response.history.to_s
+#puts "body:"    + response.body
