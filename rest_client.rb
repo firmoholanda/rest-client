d14c2788c8f0f3a32065c0cf0dd131bf1ace443b
firmo holanda <firmo@viize.com>
2020-01-22 17:43:29 -0300

    added body dump file

--- rest_client.rb
+++ rest_client.rb
@@ -2,8 +2,10 @@ require 'rest-client'
 
 response = RestClient.get "https://www.bing.com/search?q=rails"
 
-puts "code:"    + response.code.to_s
+#puts "code:"    + response.code.to_s
 #puts "headers:" + response.headers.to_s
 #puts "cookies:" + response.cookies.to_s
 #puts "history:" + response.history.to_s
 #puts "body:"    + response.body
+
+File.write('body.html', response.body.to_s)
