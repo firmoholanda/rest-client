9c5086a8632944062f4e3d86e77b9ee367517e7c
firmo holanda <firmo@viize.com>
2020-01-23 11:39:27 -0300

    Update rest_client.rb

--- rest_client.rb
+++ rest_client.rb
@@ -19,9 +19,6 @@ class BingSearch
   def show
     puts 'code:'    + @response.code.to_s
     puts 'headers:' + @response.headers.to_s
-  end
-
-  def show2
     puts 'cookies:' + @response.cookies.to_s
     puts 'history:' + @response.history.to_s
     puts 'body:'    + @response.body
